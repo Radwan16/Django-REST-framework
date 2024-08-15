2fa0d11bd19474d5fda7b0a4e4c8697c630a278c
Radwan16 <32171764+Radwan16@users.noreply.github.com>
2024-08-15 22:34:07 +0200

    Many to one relationship methods

--- api/serializers.py
+++ api/serializers.py
@@ -15,7 +15,15 @@ class ExtraInfoSerializer(serializers.ModelSerializer):
 class RecenzjaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Recenzja
-        fields = ['opis','gwiazdki']
+        fields ='__all__'
+        # depth = 2
+        # read_only_fields = ('film','id')
+
+    def update(self, instance,validated_data):
+        instance.opis = validated_data.get('opis',instance.opis)
+        instance.gwiazdki = validated_data.get('gwiazdki',instance.gwiazdki)
+        instance.save()
+        return  instance
 
 class FilmSerializer(serializers.ModelSerializer):
     extra_info = ExtraInfoSerializer(many=False)
